2902580510eba31c5645995f8cb24edd111bc485
BeneArp <benedetta.arpino@gmail.com>
2024-08-22 19:59:53 +0200

    build: basic layout

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.4/axios.min.js" integrity="sha512-6VJrgykcg/InSIutW2biLwA1Wyq+7bZmMivHw19fI+ycW0jIjsadm8wKQQLlfv3YUS4owfMDlZU38NtaAK6fSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="container-md text-center mt-5">
+            <h1>Todo List</h1>
+
+            <div class="ms-container">
+                <ul>
+                    <li class="row flex-nowrap justify-content-between gx-0">
+                        <div class="col-auto">
+                            <span>Ciao</span>
+                        </div>
+                        
+                        <div class="col-auto">
+                            <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                        </div>
+                        
+                    </li>
+
+                    <li class="row flex-nowrap justify-content-between gx-0">
+                        <div class="col-auto">
+                            <span>Ciao</span>
+                        </div>
+                        
+                        <div class="col-auto">
+                            <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                        </div>
+                        
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
